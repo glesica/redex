dec1a319ad0c22be0d5a02e86b4eb021c7523246
George Lesica <george@lesica.com>
2016-09-24 13:37:09 -0600

    Switch to more traditional parsing strategy.

--- lib/resp/parser/parser.ex
+++ lib/resp/parser/parser.ex
@@ -1,31 +1,106 @@
 defmodule Redex.RESP.Parser do
-  def parse([<< "+", d::binary >>, ""]), do: {:simple_string, d}
-  def parse([<< "-", d::binary >>, ""]), do: {:error, d}
-  def parse([<< ":", d::binary >>, ""]), do: {:integer, String.to_integer(d)}
-  def parse([<< "$", l::binary >>, d, ""]) do
-    case Integer.parse(l) do
-      {len, ""} ->
-        if len == String.length(d) do
-          {:bulk_string, d}
-        else
-          :error
+  defp consume_bstr(d, l) when is_binary(d), do: consume_bstr(d, l, "")
+  defp consume_bstr(<< "\r\n", t::binary >>, 0, s), do: {s, t}
+  defp consume_bstr(<< c::binary - size(1), t::binary >>, l, s) do
+    consume_bstr(t, l - 1, s <> c)
+  end
+  defp consume_bstr("", _, s), do: {:error_bstr, {:eof, s}, ""}
+
+  defp consume_int(d) when is_binary(d), do: consume_int(d, "")
+  defp consume_int(<< "\r\n", t::binary >>, s) do
+    case Integer.parse(s) do
+      {i, ""} ->
+        {i, t}
+      :error ->
+        {:error_int, {:invalid_int, s}, t}
+    end
+  end
+  defp consume_int(<< c::binary - size(1), t::binary >>, s) do
+    consume_int(t, s <> c)
+  end
+  defp consume_int("", s), do: {:error_int, {:eof, s}, ""}
+
+  defp consume_str(d) when is_binary(d), do: consume_str(d, "")
+  defp consume_str(<< "\r\n", t::binary >>, s), do: {s, t}
+  defp consume_str(<< c::binary - size(1), t::binary >>, s) do
+      consume_str(t, s <> c)
+  end
+  defp consume_str("", s), do: {:error_str, {:eof, s}, ""}
+
+  defp parse_term(<< "+", t::binary >>) do
+    case consume_str(t) do
+      {s, tt} when is_binary(s) ->
+        {{:str, s}, tt}
+      {:error_str, {:eof, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(<< "-", t::binary >>) do
+    case consume_str(t) do
+      {s, tt} when is_binary(s) ->
+        {{:err, s}, tt}
+      {:error_str, {:eof, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(<< ":", t::binary >>) do
+    case consume_int(t) do
+      {i, tt} when is_integer(i) ->
+        {{:int, i}, tt}
+      {:error_int, {:eof, _}, _} ->
+        :error
+      {:error_int, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(<< "$", t::binary >>) do
+    case consume_int(t) do
+      {-1, tt} ->
+        {{:bstr, :null}, tt}
+      {l, tt} when is_integer(l) ->
+        case consume_bstr(tt, l) do
+          {s, ttt} when is_binary(s) ->
+            {{:bstr, s}, ttt}
+          {:error_bstr, {:eof, _}, _} ->
+            :error
         end
+      {:error_int, {:eof, _}, _} ->
+        :error
+      {:error_int, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(<< "*", t::binary >>) do
+    case consume_int(t) do
+      {l, tt} when is_integer(l) ->
+        parse_array(tt, l, [])
+      {:error_int, {:eof, _}, _} ->
+        :error
+      {:error_int, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(_), do: :error
+
+  defp parse_array(s, 0, terms), do: {{:array, terms}, s}
+  defp parse_array("", _, _), do: :error
+  defp parse_array(s, l, terms) do
+    case parse_term(s) do
+      {term, t} ->
+        parse_array(t, l - 1, terms ++ [term])
       :error ->
         :error
     end
   end
-  def parse([<< "$", l::binary >>, ""]) do
-    case Integer.parse(l) do
-      {len, ""} ->
-        if len == -1 do
-          {:bulk_string, :null}
-        else
-          :error
-        end
+
+  def parse(s) do
+    case parse_term(s) do
       :error ->
+        :error
+      {term, ""} ->
+        term
+      {_, _} ->
         :error
     end
   end
-  def parse(cmd) when is_list(cmd), do: :error
-  def parse(cmd), do: String.split(cmd, "\r\n") |> parse
 end
--- test/resp/parser/parser_test.exs
+++ test/resp/parser/parser_test.exs
@@ -13,48 +13,73 @@ defmodule Redex.RESP.ParserTest do
   end
 
   test "should parse simple strings" do
-    assert parse("+OK\r\n") == {:simple_string, "OK"}
+    assert parse("+OK\r\n") == {:str, "OK"}
   end
 
   test "should parse simple strings with spaces" do
-    assert parse("+OK again\r\n") == {:simple_string, "OK again"}
+    assert parse("+OK again\r\n") == {:str, "OK again"}
   end
 
   test "should parse errors" do
-    assert parse("-Error\r\n") == {:error, "Error"}
+    assert parse("-Error\r\n") == {:err, "Error"}
   end
 
   test "should parse multi-word errors" do
-    assert parse("-Error again\r\n") == {:error, "Error again"}
+    assert parse("-Error again\r\n") == {:err, "Error again"}
   end
 
   test "should parse integers" do
-    assert parse(":100\r\n") == {:integer, 100}
+    assert parse(":100\r\n") == {:int, 100}
   end
 
   test "should parse negative integers" do
-    assert parse(":-100\r\n") == {:integer, -100}
+    assert parse(":-100\r\n") == {:int, -100}
   end
 
   test "should parse zero integers" do
-    assert parse(":0\r\n") == {:integer, 0}
+    assert parse(":0\r\n") == {:int, 0}
   end
 
   test "should parse bulk strings" do
-    assert parse("$5\r\nhello\r\n") == {:bulk_string, "hello"}
+    assert parse("$5\r\nhello\r\n") == {:bstr, "hello"}
   end
 
   test "should parse an empty bulk string" do
-    assert parse("$0\r\n\r\n") == {:bulk_string, ""}
+    assert parse("$0\r\n\r\n") == {:bstr, ""}
   end
 
   test "should parse the null bulk string" do
-    assert parse("$-1\r\n") == {:bulk_string, :null}
+    assert parse("$-1\r\n") == {:bstr, :null}
   end
 
   test "should return :error on bulk string length mismatch" do
     assert parse("$1\r\nab\r\n") == :error
     assert parse("$2\r\na\r\n") == :error
+  end
+
+  test "should parse an empty array" do
+    assert parse("*0\r\n") == {:array, []}
+  end
+
+  test "should parse an array with homogeneous simple types" do
+    assert parse("*2\r\n:1\r\n:2\r\n") == {:array, [{:int, 1}, {:int, 2}]}
+  end
+
+  test "should parse an array with mixed simple types" do
+    assert parse("*2\r\n+OK\r\n:1\r\n") == {:array, [{:str, "OK"}, {:int, 1}]}
+  end
+
+  test "should parse an array that includes bulk strings" do
+    assert parse("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n") == 
+      {:array, [{:bstr, "foo"}, {:bstr, "bar"}]}
+  end
+
+  test "should return :error on array length mismatch" do
+    assert parse("*2\r\n+OK\r\n") == :error
+  end
+
+  test "should return :error on array on element parse error" do
+    assert parse("*1\r\nOK\r\n") == :error
   end
 end
 
