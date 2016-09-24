2b08a2d1e837579a0b483a6194824ed0aa5a6421
George Lesica <george@lesica.com>
2016-09-24 14:19:24 -0600

    Clean up parser code and improve variable names.

--- lib/resp/parser/parser.ex
+++ lib/resp/parser/parser.ex
@@ -1,98 +1,8 @@
 defmodule Redex.RESP.Parser do
-  defp consume_bstr(d, l) when is_binary(d), do: consume_bstr(d, l, "")
-  defp consume_bstr(<< "\r\n", t::binary >>, 0, s), do: {s, t}
-  defp consume_bstr(<< c::binary - size(1), t::binary >>, l, s) do
-    consume_bstr(t, l - 1, s <> c)
-  end
-  defp consume_bstr("", _, s), do: {:error_bstr, {:eof, s}, ""}
-
-  defp consume_int(d) when is_binary(d), do: consume_int(d, "")
-  defp consume_int(<< "\r\n", t::binary >>, s) do
-    case Integer.parse(s) do
-      {i, ""} ->
-        {i, t}
-      :error ->
-        {:error_int, {:invalid_int, s}, t}
-    end
-  end
-  defp consume_int(<< c::binary - size(1), t::binary >>, s) do
-    consume_int(t, s <> c)
-  end
-  defp consume_int("", s), do: {:error_int, {:eof, s}, ""}
-
-  defp consume_str(d) when is_binary(d), do: consume_str(d, "")
-  defp consume_str(<< "\r\n", t::binary >>, s), do: {s, t}
-  defp consume_str(<< c::binary - size(1), t::binary >>, s) do
-      consume_str(t, s <> c)
-  end
-  defp consume_str("", s), do: {:error_str, {:eof, s}, ""}
-
-  defp parse_term(<< "+", t::binary >>) do
-    case consume_str(t) do
-      {s, tt} when is_binary(s) ->
-        {{:str, s}, tt}
-      {:error_str, {:eof, _}, _} ->
-        :error
-    end
-  end
-  defp parse_term(<< "-", t::binary >>) do
-    case consume_str(t) do
-      {s, tt} when is_binary(s) ->
-        {{:err, s}, tt}
-      {:error_str, {:eof, _}, _} ->
-        :error
-    end
-  end
-  defp parse_term(<< ":", t::binary >>) do
-    case consume_int(t) do
-      {i, tt} when is_integer(i) ->
-        {{:int, i}, tt}
-      {:error_int, {:eof, _}, _} ->
-        :error
-      {:error_int, {:invalid_int, _}, _} ->
-        :error
-    end
-  end
-  defp parse_term(<< "$", t::binary >>) do
-    case consume_int(t) do
-      {-1, tt} ->
-        {{:bstr, :null}, tt}
-      {l, tt} when is_integer(l) ->
-        case consume_bstr(tt, l) do
-          {s, ttt} when is_binary(s) ->
-            {{:bstr, s}, ttt}
-          {:error_bstr, {:eof, _}, _} ->
-            :error
-        end
-      {:error_int, {:eof, _}, _} ->
-        :error
-      {:error_int, {:invalid_int, _}, _} ->
-        :error
-    end
-  end
-  defp parse_term(<< "*", t::binary >>) do
-    case consume_int(t) do
-      {l, tt} when is_integer(l) ->
-        parse_array(tt, l, [])
-      {:error_int, {:eof, _}, _} ->
-        :error
-      {:error_int, {:invalid_int, _}, _} ->
-        :error
-    end
-  end
-  defp parse_term(_), do: :error
-
-  defp parse_array(s, 0, terms), do: {{:array, terms}, s}
-  defp parse_array("", _, _), do: :error
-  defp parse_array(s, l, terms) do
-    case parse_term(s) do
-      {term, t} ->
-        parse_array(t, l - 1, terms ++ [term])
-      :error ->
-        :error
-    end
-  end
-
+  @doc """
+  Parse the given RESP string into tagged tuples of native Elixir
+  data structures.
+  """
   def parse(s) do
     case parse_term(s) do
       :error ->
@@ -100,6 +10,123 @@ defmodule Redex.RESP.Parser do
       {term, ""} ->
         term
       {_, _} ->
+        :error
+    end
+  end
+
+  # Consume some number of characters from the input and
+  # return them as a string. The string returned may
+  # contain one or more CRLFs.
+  defp consume_bstr(data, n), do: consume_bstr(data, n, "")
+  defp consume_bstr(<< "\r\n", new_data::binary >>, 0, bstr_chars) do
+    {bstr_chars, new_data}
+  end
+  defp consume_bstr(<< next_char::binary - size(1), new_data::binary >>, n, bstr_chars) do
+    consume_bstr(new_data, n - 1, bstr_chars <> next_char)
+  end
+  defp consume_bstr("", _, bstr_chars) do
+    {:error, {:eof, bstr_chars}, ""}
+  end
+
+  # Consume characters until the next CRLF and return them,
+  # interpreted as an integer.
+  defp consume_int(data), do: consume_int(data, "")
+  defp consume_int(<< "\r\n", new_data::binary >>, int_chars) do
+    case Integer.parse(int_chars) do
+      {parsed_int, ""} ->
+        {parsed_int, new_data}
+      :error ->
+        {:error, {:invalid_int, int_chars}, new_data}
+    end
+  end
+  defp consume_int(<< next_char::binary - size(1), new_data::binary >>, int_chars) do
+    consume_int(new_data, int_chars <> next_char)
+  end
+  defp consume_int("", int_chars), do: {:error, {:eof, int_chars}, ""}
+
+  # Consume a string from the input, reading up to the
+  # next CRLF. The string returned will never include a
+  # CRLF.
+  defp consume_str(data), do: consume_str(data, "")
+  defp consume_str(<< "\r\n", new_data::binary >>, str_chars) do
+    {str_chars, new_data}
+  end
+  defp consume_str(<< next_char::binary - size(1), new_data::binary >>, str_chars) do
+      consume_str(new_data, str_chars <> next_char)
+  end
+  defp consume_str("", str_chars), do: {:error, {:eof, str_chars}, ""}
+
+  # Simple String
+  defp parse_term(<< "+", new_data::binary >>) do
+    case consume_str(new_data) do
+      {str, new_data} when is_binary(str) ->
+        {{:str, str}, new_data}
+      {:error, {:eof, _}, _} ->
+        :error
+    end
+  end
+
+  # Error
+  defp parse_term(<< "-", new_data::binary >>) do
+    case consume_str(new_data) do
+      {err, new_data} when is_binary(err) ->
+        {{:err, err}, new_data}
+      {:error, {:eof, _}, _} ->
+        :error
+    end
+  end
+
+  # Integer
+  defp parse_term(<< ":", new_data::binary >>) do
+    case consume_int(new_data) do
+      {int, new_data} when is_integer(int) ->
+        {{:int, int}, new_data}
+      {:error, {:eof, _}, _} ->
+        :error
+      {:error, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+
+  # Bulk String
+  defp parse_term(<< "$", new_data::binary >>) do
+    case consume_int(new_data) do
+      {-1, new_data} ->
+        {{:bstr, :null}, new_data}
+      {bstr_len, new_data} when is_integer(bstr_len) ->
+        case consume_bstr(new_data, bstr_len) do
+          {bstr, new_data} when is_binary(bstr) ->
+            {{:bstr, bstr}, new_data}
+          {:error, {:eof, _}, _} ->
+            :error
+        end
+      {:error, {:eof, _}, _} ->
+        :error
+      {:error, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+
+  # Array
+  defp parse_term(<< "*", new_data::binary >>) do
+    case consume_int(new_data) do
+      {array_len, new_data} when is_integer(array_len) ->
+        parse_array(new_data, array_len, [])
+      {:error, {:eof, _}, _} ->
+        :error
+      {:error, {:invalid_int, _}, _} ->
+        :error
+    end
+  end
+  defp parse_term(_), do: :error
+
+  defp parse_array(data, 0, array_els), do: {{:array, array_els}, data}
+  defp parse_array("", _, _), do: :error
+  defp parse_array(data, array_len, array_els) do
+    case parse_term(data) do
+      {array_el, new_data} ->
+        parse_array(new_data, array_len - 1, array_els ++ [array_el])
+      :error ->
         :error
     end
   end
