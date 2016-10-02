62700def52679a7a29bb08ed5f37d368af75c443
George Lesica <george@lesica.com>
2016-10-02 02:04:11 -0600

    Clean up REPL a little.

--- lib/client/main.ex
+++ lib/client/main.ex
@@ -1,14 +1,29 @@
 defmodule Redex.Client.Main do
+  @prompt_string "REDEX > "
+
   def main(_args) do
     {:ok, pid} = Redex.Client.start_link()
     prompt(pid)
   end
 
   defp prompt(pid) do
-    cmd = IO.gets("redex >") |> String.trim
-    {_, resp} = Redex.Client.command(pid, cmd)
-    IO.puts(resp)
-    prompt(pid)
+    cmd = IO.gets(@prompt_string)
+    case cmd do
+      :eof ->
+        quit()
+      "exit\n" ->
+        quit()
+      "quit\n" ->
+        quit()
+      _ ->
+        {_, resp} = Redex.Client.command(pid, cmd |> String.trim)
+        IO.puts(resp)
+        prompt(pid)
+    end
+  end
+
+  defp quit() do
+    IO.puts("Goodbye!")
   end
 end
 
